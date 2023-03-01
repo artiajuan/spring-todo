7bf82fbaf3de1203a20fe0edf055eeda169bc667
Juan Bautista Artia <jartia@alumnos.exa.unicen.edu.ar>
2023-03-01 10:30:25 -0300

    primer commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, description, done, target_date, username)
+values (1000, 'Learn Spring Boot', false, current_date(), 'in28minutes');
+
+insert into todo(id, description, done, target_date, username)
+values (1001, 'Learn Spring MVC', false, current_date(), 'in28minutes');
+
+insert into todo(id, description, done, target_date, username)
+values (1002, 'Learn to Dance', false, current_date(), 'in28minutes');
